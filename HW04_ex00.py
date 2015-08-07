ead035220ea4514aea0c0c5ac45d2894ed9fd8b1
Reema Naqvi <reemanaqvi@calvisitor-10-105-174-15.CalVisitor.1918.Berkeley.EDU>
2015-08-07 14:18:21 -0700

    exercise hw04_00

--- HW04_ex00.py
+++ HW04_ex00.py
@@ -11,9 +11,36 @@
 #         - then ends the program
 ################################################################################
 # Imports
-
+import random
+import sys
 
 # Body
+
+nr = (random.randint(1,25))
+count= 0
+while (count < 5):
+	guess = raw_input ('You have 5 attempts to guess a number between 1-25: ')
+	try: 
+		guess_int = int(guess)         # note to self: if its actually an int, itll store it in guess_int or else throw an exception if not
+	except:
+		print 'Only enter a number'
+		count = count +1
+	else:
+		if guess_int == nr:
+				print "Congrats! You guessed the right number ", guess
+				sys.exit()
+		else:
+			if guess_int < nr:
+				print "Go higher"
+				count = count +1
+			else:
+				print "Go lower"
+				count = count +1
+print 'Oops, the correct number was: ', nr
+
+	
+
+
 
 
 
